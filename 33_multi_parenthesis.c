c75ce3c4be2c0f6bb69e6ab4f4c571c1a1fc9cec
mastercooder <tejesh.patel256@gmail.com>
2024-01-26 18:37:39 +0530

    26/01/2024

--- 33_multi_parenthesis.c
+++ 33_multi_parenthesis.c
@@ -26,7 +26,7 @@ int isFull(struct Stack *ptr)
     }
 }
 
-void push(struct Stack *ptr, int value);
+void push(struct Stack *ptr, int value)
 {
     if(isFull(ptr)){
         printf("Stack Overflow\n");
@@ -47,10 +47,68 @@ char pop(struct Stack *ptr)
     }
 }
 
-int parenthisisMatch(struct Stack *ptr, int size)
+int match(char a, char b)
+{
+    if (a == '(' && b == ')'){
+        return 1;
+    } else if (a == '{' && b == '}') {
+        return 1;
+    } else if (a == '[' && b == ']') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+
+int parenthisisMatch(char *exp, int size)
 {
     struct Stack *sp = (struct Stack*)malloc(sizeof(struct Stack));
     sp->size = size;
     sp->top = -1;
     sp->arr = (char*)malloc(sp->size*sizeof(char));
+    char popped_ch;
+
+    for (int i = 0; exp[i]!='\0'; i++)
+    {
+        if(exp[i] == '(' || exp[i] == '[' || exp[i] == '{'){
+            push(sp, exp[i]);
+        } else if(exp[i] == ')' || exp[i] == ']' || exp[i] == '}'){
+            if(isEmpty(sp)){
+                return 0;
+            } else{
+                popped_ch = pop(sp);
+                if(!match(popped_ch, exp[i])){
+                    return 0;
+                }
+            }
+        }
+    }
+    
+    if(isEmpty(sp)){
+        return 1;
+    } else{
+        return 0;
+    }
+}
+
+int main(){
+    int size;
+
+    printf("Enter the size of your expression: ");
+    scanf("%d", &size);
+
+    while (1)
+    {
+        char query[size];
+        printf("Enter the Expression: ");
+        scanf("%s", query);
+
+        if(parenthisisMatch(query, size)){
+            printf("Expression Balanced\n");
+        } else{
+            printf("Expression Not Balanced\n");
+        }
+    }
+    return 0;
 }
