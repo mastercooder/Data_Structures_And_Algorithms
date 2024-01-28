575df116db7c78c1263c53527327c09e4f3cd9b0
mastercooder <tejesh.patel256@gmail.com>
2024-01-28 14:27:53 +0530

    28/01/2024

--- 37_infix_to_postfix.c
+++ 37_infix_to_postfix.c
@@ -52,17 +52,71 @@ char pop(struct Stack *ptr)
     }
 }
 
+int precedence(char ch)
+{
+    if(ch=='*' || ch=='/'){
+        return 2;
+    } else if(ch=='+' || ch=='-'){
+        return 1;
+    } else{
+        return 0;
+    }
+}
+
+int isOperator(char ch)
+{
+    if(ch=='*' || ch=='/' || ch=='+' || ch=='-'){
+        return 1;
+    } else{
+        return 0;
+    }
+}
+
 char *infixtoPostfix(char *infix, int size)
 {
     struct Stack *sp = (struct Stack*)malloc(sizeof(struct Stack));
     sp->size = size;
     sp->top = -1;
     sp->arr = (char*)malloc(sp->size*sizeof(char));
-    char *postFix = (char*)malloc((strlen(infix)+1))sizeof(char);
-
+    char *postFix = (char*)malloc((strlen(infix)+1)*sizeof(char));
+    int i = 0;
+    int j = 0;
+    while (infix[i]!='\0')
+    {
+        if(!isOperator(infix[i])){
+            postFix[j] = infix[i];
+            i++;
+            j++;
+        } else{
+            if(precedence(infix[i])>precedence(stackTop(sp))){
+                push(sp, infix[i]);
+                i++;
+            } else{
+                postFix[j] = pop(sp);
+                j++;
+            }
+        }
+    }
+    while (!isEmpty(sp)){
+        postFix[j] = pop(sp);
+        j++;
+    }
+    postFix = '\0';
+    return postFix;
 }
 
 int main(){
-    char query[50] = "2-4+5";
+    int size;
+    printf("Enter the size: ");
+    scanf("%d", &size);
+
+    char query[size];
+    printf("Enter the Expression: ");
+    scanf("%d", query);
+    printf("\n");
+    printf("The Infix is: %s\n", query);
+    printf("The postFix is: %s", infixtoPostfix(query, size));
+    
+    
     return 0;
 }
